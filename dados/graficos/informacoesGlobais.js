d4cd8c99860ce7641018c4423ed6419f590e0834
zavatinilara <127403856+zavatinilara@users.noreply.github.com>
2024-09-16 12:28:30 -0300

    Add files via upload

---
+++ dados/graficos/informacoesGlobais.js
@@ -0,0 +1,9 @@
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json";
+
+async function vizualizarInformacoesGlobais(){
+    const res = await fetch(url);
+    const dados = res.json();
+    console.log(dados);
+}
+
+vizualizarInformacoesGlobais();
