c060a66f5daac12e3b2bda2652988c120be92415
zavatinilara <127403856+zavatinilara@users.noreply.github.com>
2024-10-21 11:59:04 -0300

    Delete dados directory

--- dados/graficos/informacoesGlobais.js
+++
@@ -1,9 +0,0 @@
-const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json";
-
-async function vizualizarInformacoesGlobais(){
-    const res = await fetch(url);
-    const dados = res.json();
-    console.log(dados);
-}
-
-vizualizarInformacoesGlobais();
